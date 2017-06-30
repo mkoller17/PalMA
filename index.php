894f68b1ff6d6ea5cac5a268f733659492a005e0
root <root@lc02.bib.uni-mannheim.de>
2017-06-30 15:25:21 +0200

    Add Piwik code
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- index.php
+++ index.php
@@ -1246,4 +1246,24 @@ pollDatabase();
 
 </body> <!-- workbench -->
 
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.lc02.bib.uni-mannheim.de"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//ub-monitor.bib.uni-mannheim.de/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '21']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+
+
 </html>
